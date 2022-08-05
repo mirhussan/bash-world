f0134118d2cae639520cd1641eb31b188bd37f55
Mir Hasan <23290678+geekmir@users.noreply.github.com>
2022-08-05 12:21:54 +0500

    Update backup-delete-DO-space.sh

--- backup-delete-DO-space.sh
+++ backup-delete-DO-space.sh
@@ -15,16 +15,16 @@ LIVEMYSQLFILENAME=live_db.sql.gz
 
 for i in `seq -w 1 31`
 do
-s3cmd del s3://wpbspace/$SPACENAME/nginx/$i$DATE"_000001"/$NGINXFILENAME
-s3cmd del s3://wpbspace/$SPACENAME/nginx/$i$DATE"_120001"/$NGINXFILENAME
+s3cmd del s3://$SPACENAME/nginx/$i$DATE"_000001"/$NGINXFILENAME
+s3cmd del s3://$SPACENAME/nginx/$i$DATE"_120001"/$NGINXFILENAME
 #
-s3cmd del s3://wpbspace/$SPACENAME/code/$i$DATE"_000001"/$CODEFILENAME
+s3cmd del s3://$SPACENAME/code/$i$DATE"_000001"/$CODEFILENAME
 s3cmd del s3://wpbspace/$SPACENAME/code/$i$DATE"_120001"/$CODEFILENAME
 #
-s3cmd del s3://wpbspace/$SPACENAME/mysqldump/$i$DATE"_000001"/$STAGEMYSQLFILENAME
-s3cmd del s3://wpbspace/$SPACENAME/mysqldump/$i$DATE"_000001"/$LIVEMYSQLFILENAME
-s3cmd del s3://wpbspace/$SPACENAME/mysqldump/$i$DATE"_120001"/$LIVEMYSQLFILENAME
-s3cmd del s3://wpbspace/$SPACENAME/mysqldump/$i$DATE"_120001"/$STAGEMYSQLFILENAME
+s3cmd del s3://$SPACENAME/mysqldump/$i$DATE"_000001"/$STAGEMYSQLFILENAME
+s3cmd del s3://$SPACENAME/mysqldump/$i$DATE"_000001"/$LIVEMYSQLFILENAME
+s3cmd del s3://$SPACENAME/mysqldump/$i$DATE"_120001"/$LIVEMYSQLFILENAME
+s3cmd del s3://SPACENAME/mysqldump/$i$DATE"_120001"/$STAGEMYSQLFILENAME
 
 
 done
