5244221a25b32ba1c31aec88179924fbeb95b388
Mir Hasan <23290678+itsMirHasan@users.noreply.github.com>
2025-01-25 17:51:04 +0100

    Update installation-renv.sh

--- installation-renv.sh
+++ installation-renv.sh
@@ -2,7 +2,7 @@ Installation Steps Ruby on rail in ubuntu OS
        - apt-get update && apt-get install -y git gcc bzip2 libssl-dev libreadline-dev zlib1g-dev
 
 Installation Steps Ruby on Rail in EC2 / Redhat / Fedora / Alma 
-       - yum update && yum groupinstall "Development Tools"  && sudo yum install -y openssl-devel readline-devel zlib-devel  bzip2-devel libffi-devel
+       - yum update && yum groupinstall "Development Tools"  && sudo yum install -y openssl-devel readline-devel zlib-devel  bzip2-devel libffi-devel libyaml-devel
 
 
        - curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-installer | bash
